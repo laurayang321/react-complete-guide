32dc5d2f08cf102d5e50795576a4dcafed392abd
Laura Yang <laurayang321@gmail.com>
2021-01-27 20:37:56 -0800

    48. Adding Styling with Stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './Person.css';
+
 // Person Component is a stateless/dumb/presentational Component - because it has no internal state management
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick = {props.click}>I'm {props.name}. I'm {props.age} years old.</p>
             <p>{props.children}</p>
             <input type='text' onChange={props.changed} value={props.name}></input>
