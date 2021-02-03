eff8449212c218a4ea96fb6e3ad3cfa5d2b46287
Laura Yang <laurayang321@gmail.com>
2021-02-02 23:22:51 -0800

    60. Flexible Lists

--- src/App.js
+++ src/App.js
@@ -32,14 +32,22 @@ class App extends Component {
     })
   }
 
- nameChangedHandler = (event) => {
-  this.setState( {
-      persons: [
-        {name: 'Max', age: 28},
-        {name: event.target.value, age: 29},
-        {name: 'Stephanie', age: 27}
-      ]
-    } )
+ nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // person is a pointer object, need Spread Operator to create a JavaScript object
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} );
   }
 
   togglePersonsHandler = () => {
@@ -72,7 +80,8 @@ class App extends Component {
                 click = {() => this.deletePersonHandler(index)}
                 name={person.name} 
                 age={person.age} 
-                key={person.id}/>
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       );
