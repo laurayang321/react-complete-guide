4ae448f96c0bb12ca4f9595d1f00a33c37f9ee32
Laura Yang <laurayang321@gmail.com>
2021-02-04 19:38:59 -0800

    69. Using Radium for Media Queries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person'; // js extension can be ommitted due to the build workflow
 
 // App Component is a stateful Component
@@ -116,14 +116,16 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App.</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-        style={style}
-        onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App.</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
   // the lowercase is reserved for native HTML in JSX like div, so components usually use uppercase. 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,13 @@ import './Person.css';
 
 // Person Component is a stateless/dumb/presentational Component - because it has no internal state management
 const person = (props) => {
+    // if screen > 500px: Person div stay as 450px width
+    // if screen < 500px: Person div stay as 60% of screen width
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    };
     return (
         <div className="Person">
             <p onClick = {props.click}>I'm {props.name}. I'm {props.age} years old.</p>
