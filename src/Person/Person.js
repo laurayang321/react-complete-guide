f772bd3a35757cebd9a4e09f020a3e6a65b5dfc4
Laura Yang <laurayang321@gmail.com>
2021-02-04 19:50:19 -0800

    69. Using Radium in Media Queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -12,7 +12,7 @@ const person = (props) => {
         }
     };
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick = {props.click}>I'm {props.name}. I'm {props.age} years old.</p>
             <p>{props.children}</p>
             <input type='text' onChange={props.changed} value={props.name}></input>
