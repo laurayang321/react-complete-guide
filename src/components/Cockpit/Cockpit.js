b1001d655941fa641ccd8841e49baeb98dad8119
Laura Yang <laurayang321@gmail.com>
2021-02-17 15:50:29 -0800

    95. Controlling useEffect Behavior

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -8,7 +8,14 @@ const cockpit = (props) => {
         // when type, re-rendered app.js, state changes, it gets run
         console.log('[Cockpit.js] useEffect');
         // Http request ...
-    });
+        setTimeout(() => {
+            alert('Save data to cloud!');
+        }, 1000);
+    }, [props.persons]); // only execute when person is changed
+    // if array is empty, then it only exc once like componentDidMount
+
+    // useEffect can be used multiple times
+    // useEffect();
 
     //let classes = ['red', 'bold'].join(' '); //use join to convert array to string
     let assignedClasses = [];
